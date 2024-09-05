7fe9ac7acbb48be0d54b86df43ea3bc4c9971535
Lenrow <leoeberle@gmx.de>
2024-09-05 16:17:33 +0200

    Fix Issue with vectors not accepting empty strings

--- addons/object_creator/Scripts/InputScripts/vector_input.gd
+++ addons/object_creator/Scripts/InputScripts/vector_input.gd
@@ -25,10 +25,13 @@ func initialize_input(property_dict: Dictionary):
 	type_label.text = return_type_string(input_type)
 	input_node.create_vector_UI(input_type)
 	input_node.accept_empty_inputs = accept_empty_inputs
+	
 
 func attempt_submit() -> Variant:
 	var return_value = null
 	var temp_value = input_node.return_input()
+	print("Tempvalue is ", temp_value)
+	print("and the bool is ", temp_value == null)
 	if not check_input_range(temp_value) or temp_value == null:
 		return_value = null
 		show_input_warning()
