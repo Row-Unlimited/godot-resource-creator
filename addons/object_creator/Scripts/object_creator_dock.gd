5d2695311aa10ee5abc5b1de1c0f89bc04d30f4e
Lenrow <leoeberle@gmx.de>
2024-11-13 15:22:33 +0100

    Properly implement as Plugin and change location to Main Screen

--- addons/object_creator/Scripts/object_creator_dock.gd
+++ addons/object_creator/Scripts/object_creator_dock.gd
@@ -1,6 +1,6 @@
 @tool
 extends EditorPlugin
-const scenePath = "res://addons/object_creator/Scenes/startup_scene.tscn"
+const scenePath = "res://addons/object_creator/Scenes/main_scene.tscn"
 const PLUGIN_CONFIG_PATH = "res://addons/object_creator/PluginConfig.tres"
 var dock
 var plugin_config = preload(PLUGIN_CONFIG_PATH)
@@ -8,23 +8,16 @@ var plugin_config = preload(PLUGIN_CONFIG_PATH)
 func _enter_tree():
 	dock = preload(scenePath).instantiate()
 	
-	if plugin_config.is_main_dock:
-		EditorInterface.get_editor_main_screen().add_child(dock)
-	else:
-		add_control_to_dock(DOCK_SLOT_LEFT_UL, dock)
+	EditorInterface.get_editor_main_screen().add_child(dock)
 	
-	dock.start_creation_process()
 	_make_visible(false)
 
-
 func _has_main_screen():
-	return plugin_config.is_main_dock
-
+	return true
 
 func _make_visible(visible):
 	if dock:
 		dock.visible = visible
-
 
 func _get_plugin_name():
 	return "Object Creator Plugin"
